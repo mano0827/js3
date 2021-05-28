95982ad618dfa9601f1a0ad04fa91a99ae34e756
mano0827 <au.dugout@gmail.com>
2021-05-28 14:00:29 +0900

    JS3課題

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 
   const tableBody = document.getElementById('todo-body')
 
-
   document.querySelector('button').addEventListener('click', () => {
+
     const tableRecords = document.createElement('tr');
     const tableId = document.createElement('td');
     const tableComment = document.createElement('td');
@@ -12,23 +12,25 @@
     const tableAction = document.createElement('button');
     const text = document.getElementById('text');
     const btn = document.getElementById('btn');
-    tableStatus.textContent = '作業中';
+    // tableStatus.textContent = '作業中';
     tableAction.textContent = '削除';
+    // tableBody.appendChild(tableRecords);
+    // tableRecords.appendChild(tableId);
+    // tableRecords.appendChild(tableComment);
+    // tableRecords.appendChild(tableStatus);
+    // tableRecords.appendChild(tableAction);
 
-    tableBody.appendChild(tableRecords);
-    tableRecords.appendChild(tableId);
-    tableRecords.appendChild(tableComment);
-    tableRecords.appendChild(tableStatus);
-    tableRecords.appendChild(tableAction);
+    const todos = [];
+    const todo = {};
+    todo.task = text.value;
+    todo.textContent = '作業中'
+    todo.del = tableAction;
+    todos.push(todo);
+    text.value = '';
 
-    tableComment.innerHTML = text.value;
-    text.value = "";
+    // tableComment.innerHTML = text.value;
+    // text.value = "";
 
   });
 }
-
-
-
-
-
-
+義t
