ad4f06e9cb46b2c2d683b1505f6c36f216e928e9
mano0827 <au.dugout@gmail.com>
2021-06-01 11:54:57 +0900

    js3-1課題

--- js/main.js
+++ js/main.js
@@ -2,34 +2,39 @@
 {
 
   const tableBody = document.getElementById('todo-body')
+  const todos = [];
+
 
   document.querySelector('button').addEventListener('click', () => {
 
-    const tableRecords = document.createElement('tr');
-    const tableId = document.createElement('td');
     const tableComment = document.createElement('td');
-    const tableStatus = document.createElement('button');
-    const tableAction = document.createElement('button');
     const text = document.getElementById('text');
     const btn = document.getElementById('btn');
-    // tableStatus.textContent = '作業中';
-    tableAction.textContent = '削除';
-    // tableBody.appendChild(tableRecords);
-    // tableRecords.appendChild(tableId);
-    // tableRecords.appendChild(tableComment);
-    // tableRecords.appendChild(tableStatus);
-    // tableRecords.appendChild(tableAction);
 
-    const todos = [];
     const todo = {};
-    todo.task = text.value;
-    todo.textContent = '作業中'
-    todo.del = tableAction;
+    todo.id = todos.length;
+    todo.tableComment = text.value;
+    todo.tableStatus = '作業中'
+    todo.tableAction = '削除'
+
+    const tr = document.createElement('tr');
+    for (const content in todo) {
+      const td = document.createElement('td');
+      td.textContent = todo[content];
+      tr.appendChild(td);
+    }
+
+    tableBody.append(tr);
+
+    const statusButton = document.createElement('button');
+    statusButton.textContent = todo.tableStatus;
+
     todos.push(todo);
     text.value = '';
-
-    // tableComment.innerHTML = text.value;
-    // text.value = "";
+    // for (let i = tableId; i < todos.length; i++) {
+    //   todos[i].id = i
+    // }
 
   });
+
 }
